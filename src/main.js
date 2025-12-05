36fd0a76cb38306b45645d44226b2c0f4da52d85
smayank9870-hue <smayank9870@gmail.com>
2025-12-05 19:27:40 +0530

    Updated index.html

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './style.css'
+import './js/script.js'
+
+// Import jQuery and plugins
+import $ from 'jquery'
+window.jQuery = window.$ = $
+
+// Your app initialization code will go here
+console.log('Stylish Online Shoe Store - Vite version loaded!')
